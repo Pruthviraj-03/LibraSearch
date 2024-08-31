14844a6c854aa5feaf6c1010e0c680eb825e1c2e
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-08-31 12:28:52 +0530

    rounting part done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, Books } from "./pages/index";
 
 const App = () => {
   return (
     <div>
-      <span>Hello Raj !</span>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/books" element={<Books />} />
+      </Routes>
     </div>
   );
 };
