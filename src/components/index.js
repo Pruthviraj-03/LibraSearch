f9b19c9f5a21459f805fa6b9356787d3195f297b
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-08-31 14:26:51 +0530

    fixed some home page small bugs.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header.jsx";
+export { default as Button } from "./Button.jsx";
+export { default as Hero } from "./Hero.jsx";
+export { default as LimBooks } from "./LimBooks.jsx";
+export { default as ScrollToTop } from "./ScrollToTop.jsx";
+export { default as Footer } from "./Footer.jsx";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Footer from "../components/Footer";
-import LimBooks from "../components/LimBooks";
-import ScrollToTop from "../components/ScrollToTop";
+import {
+  Header,
+  Hero,
+  LimBooks,
+  ScrollToTop,
+  Footer,
+} from "../components/index";
 
 const Home = () => {
   return (
